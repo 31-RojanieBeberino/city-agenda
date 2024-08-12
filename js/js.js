3c36c79a694343ab002fd7534f19ce84f7f4fbd7
Roj Beberino <rojaniebeberino0206@gmail.com>
2024-08-12 20:40:07 +0800

    done

--- js/js.js
+++ js/js.js
@@ -1,16 +1,2 @@
-var carouselwidth = $('.carousel-inner')[0].scrollWidth;
-var cardWidth = $('.carousel-item').width();
-
-var scrollPosition = 0;
-
-$('carousel-control-next').on('click', function(){
-    if (scrollPosition < (carouselwidth - (cardWidth * 4))){
-        scrollPosition = scrollPosition + cardWidth;
-    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
-    }
-    
-});
-
-
 
 
